7c88e8a01b1c618ace3dfb21913edb40f5531376
Maryia <maritsydik@gmail.com>
2023-10-30 15:38:51 +0100

    Feat: header development

---
+++ src/components/header/Header.js
@@ -0,0 +1,44 @@
+
+
+const Header =() => {
+    return(
+    <div>
+         <header className='' id="header">
+      <nav className=''>
+        <a href="#header" className=''>
+          <img src='{LogoIcon.src}' alt="logo" />
+        </a>
+        <ul
+          className=''
+        >
+          <li className=''>
+            <a href="#" className=''>
+              
+            </a>
+          </li>
+          <li className=''>
+            <a href="#" className=''>
+              
+            </a>
+          </li>
+          <li className=''>
+            <a href="#" className=''>
+              
+            </a>
+          </li>
+        </ul>
+        <button className=''>Contact</button>
+        <div
+          className=''
+        >
+          <div className='line1'></div>
+          <div className='line2'></div>
+          <div className='line3'></div>
+        </div>
+      </nav>
+    </header>
+    </div>
+    )
+}
+
+export default Header
