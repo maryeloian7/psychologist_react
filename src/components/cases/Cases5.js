f3e328de1fb0463b08d714e4f5317121d0cadba1
Maryia <maritsydik@gmail.com>
2023-11-07 16:25:05 +0100

    Feat: case refactoring

--- src/components/cases/Cases5.js
+++ src/components/cases/Cases5.js
@@ -1,55 +1,63 @@
-import React, {useState, useEffect, useRef} from 'react'
+import React, { useState, useEffect, useRef } from "react";
 
-import Group from '../../resources/img/Group 1.svg';
-import '../cases/Cases.css'
+import Group from "../../resources/img/Group 1.svg";
+import "../cases/Cases.css";
 
 const Cases5 = () => {
-    const [toggle, setToggle] = useState(false)
-    const [heightEl, setHeightEl] = useState();
+  const [toggle, setToggle] = useState(false);
+  const [heightEl, setHeightEl] = useState();
 
-    const refHeight = useRef()
+  const refHeight = useRef();
 
-    useEffect(() => {
-        setHeightEl(`${refHeight.current.scrollHeight}px`)
-    })
+  useEffect(() => {
+    setHeightEl(`${refHeight.current.scrollHeight}px`);
+  });
 
-    return (
-        <div className="cases_bloc block">
-            <div
-            onClick={()=>setToggle(!toggle)}
-            className="cases_item _item">
-                <span>Боюсь заявить о себе</span>
-                <img className={toggle ? 'cross_activ' : 'cross' } src={Group} alt=""/>
-            </div>
-            <div 
-            className={toggle ? "accordion__body animated" : "accordion__body "}
-            style={{height: toggle ? `${heightEl}` : "0px"}}
-            ref={refHeight}
+  const data = [
+    {
+      titles: "Запрос:",
+      info: "Клиент боится проявляться. Хочет развивать свое дело в соц сетях, но страшно показаться там не идеальной. Если и выходит в онлайн, готовится сто лет.",
+    },
+    {
+      titles: "Как работаем?",
+      info: "В работе выходим на ситуацию с отцом, который выпивал и в эти моменты сильно менялся, рядом с ним было небезопасно, не предсказуемо. Заходим в травмирующую ситуацию, выражаем все эмоции, гипертрофируем, тело расслабляется, отпускает ситуацию. ",
+    },
+    {
+      titles: "Сколько работали?",
+      info: "3 консультации.",
+    },
+    {
+      titles: "Результат:",
+      info: " Все еще страшно, но прогресс на лицо, в соц сети клиент выходит чаще, не так долго готовится.",
+    },
+  ];
+
+  return (
+    <div className="cases_bloc block">
+      <div onClick={() => setToggle(!toggle)} className="cases_item _item">
+        <span>Боюсь заявить о себе</span>
+        <img className={toggle ? "cross_activ" : "cross"} src={Group} alt="" />
+      </div>
+      <div
+        className={toggle ? "accordion__body animated" : "accordion__body "}
+        style={{ height: toggle ? `${heightEl}` : "0px" }}
+        ref={refHeight}
+      >
+        {data.map((item, i) => {
+          return (
+            <p
+              className="cases_item_tit _item_tit"
+              aria-hidden={toggle ? "true" : "false"}
+              key={i}
             >
-                <p 
-                className="cases_item_tit _item_tit"
-                aria-hidden={toggle ? "true" : "false"}>
-                    <span className="cases_titles">Запрос:</span><br/> Клиент боится проявляться. Хочет развивать свое дело в соц сетях, но страшно показаться там не идеальной. Если и выходит в онлайн, готовится сто лет.
-                </p>
-                <p 
-                className="cases_item_tit"
-                aria-hidden={toggle ? "true" : "false"}>
-                    <span className="cases_titles">Как работаем?</span> <br/> В работе выходим на ситуацию с отцом, который выпивал и в эти моменты сильно менялся, рядом с ним было небезопасно, не предсказуемо. Заходим в травмирующую ситуацию, выражаем все эмоции, гипертрофируем, тело расслабляется, отпускает ситуацию. 
-                </p>
-                <p 
-                className="cases_item_tit"
-                aria-hidden={toggle ? "true" : "false"}>
-                    <span className="cases_titles">Сколько работали? </span><br/>3 консультации.  
-                </p>
-                <p 
-                className="cases_item_tit"
-                aria-hidden={toggle ? "true" : "false"}>
-                    <span className="cases_titles">Результат:</span><br/> Все еще страшно, но прогресс на лицо, в соц сети клиент выходит чаще, не так долго готовится. 
-                </p>
-                
-            </div>          
-        </div>
-    )
-}
+              <span className="cases_titles">{item.titles}</span>
+              <br /> {item.info}
+            </p>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Cases5;
